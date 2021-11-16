f0c9346902323704e89b64799119afbdf4c0aac4
Mirko Zadravec <mzadravec@allplan-infra.com>
2021-11-16 17:00:11 +0100

    comments

--- BridgeTools/PropertyGrid/ABPropertyGrid.xaml.cs
+++ BridgeTools/PropertyGrid/ABPropertyGrid.xaml.cs
@@ -1,4 +1,15 @@
-﻿using BridgeTools.PropertyGrid.Categories;
+﻿//
+// Copyright: (c) Allplan Infrastructure 2021
+// ABPropertyGrid.cs
+//
+// Author: Mirko Zadravec
+//
+
+////////////////////////////
+// NAMESPACES AND CLASSES //
+////////////////////////////
+
+using BridgeTools.PropertyGrid.Categories;
 using System.Windows.Controls;
 
 // TODO
@@ -10,24 +21,50 @@ using System.Windows.Controls;
 //   - Lost focus when closing prop grid
 //   - Styles naming convention (for example see ABStyles)
 //   - Comments
+//   - Remove item from combo box (for example after "varies" changes to some selection)
 
 namespace BridgeTools.PropertyGrid
 {
+	//----------------------------------------------------------------------------------------------
 	/// <summary>
 	/// Interaction logic for ABPropertyGrid.xaml
 	/// </summary>
 	public partial class ABPropertyGrid : UserControl
     {
-        public ABPropertyGrid()
+		#region Properties
+
+		//----------------------------------------------------------------------------------------------
+		/// <summary>
+		/// Root container.
+		/// </summary>
+		public ABCat Root => Container;
+
+		#endregion
+
+		#region Constructor
+
+		//----------------------------------------------------------------------------------------------
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public ABPropertyGrid()
         {
             InitializeComponent();
         }
 
+		#endregion
+
+		#region Public Methods
+
+		//----------------------------------------------------------------------------------------------
+		/// <summary>
+		/// Clear the property grid items.
+		/// </summary>
 		public void Clear()
 		{
 			Container.Items.Clear();
 		}
 
-		public ABCat Root => Container;
+		#endregion
 	}
 }
